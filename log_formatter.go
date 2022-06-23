44a5b1cbcf736babf9610033f6bf7c13c0714619
Moritz Ploss <77320095+moritzploss-k@users.noreply.github.com>
2022-06-23 13:59:20 +0200

    feat: Improved API (#2)

--- log_formatter.go
+++ log_formatter.go
@@ -2,11 +2,15 @@ package metalog
 
 import (
 	"encoding/json"
+	"fmt"
 	"strings"
 	"time"
 
 	"github.com/gin-gonic/gin"
 )
+
+const levelINFO = "INFO"
+const levelERROR = "ERROR"
 
 func LogFormatter(param gin.LogFormatterParams) string { // nolint
 	if param.Latency > time.Minute {
@@ -16,26 +20,37 @@ func LogFormatter(param gin.LogFormatterParams) string { // nolint
 
 	logMessage := Get(param.Request)
 
+	var level string
+	if lev, ok := logMessage["level"]; ok {
+		level = lev.(string)
+	} else if _, ok := logMessage["error"]; ok {
+		level = levelERROR
+	} else if param.ErrorMessage != "" {
+		level = levelERROR
+		logMessage["error"] = strings.TrimSuffix(param.ErrorMessage, "\n")
+	} else {
+		level = levelINFO
+	}
+
 	logMessage["client_ip"] = param.ClientIP
 	logMessage["duration_us"] = param.Latency.Microseconds()
 	logMessage["headers"] = format(param.Request.Header)
-	logMessage["level"] = "INFO"
+	logMessage["level"] = LevelFormatter(level)
 	logMessage["method"] = param.Method
 	logMessage["module"] = "GIN"
 	logMessage["path"] = param.Request.URL.Path
 	logMessage["query"] = param.Request.URL.Query()
 	logMessage["response_body_size"] = param.BodySize
 	logMessage["status"] = param.StatusCode
-	logMessage["timestamp"] = param.TimeStamp.Format("2006-01-02T15:04:05.000+00:00")
-
-	if param.ErrorMessage != "" {
-		logMessage["message"] = strings.TrimSuffix(param.ErrorMessage, "\n")
-	}
+	logMessage[TimeKey] = param.TimeStamp.Format(TimeFormat)
 
 	jsonLog, err := json.Marshal(logMessage)
 
 	if err != nil {
-		return "{\"level\": \"ERROR\", \"module\": \"metalog\", \"message\": \"failed to convert log message to JSON\"}\n"
+		return fmt.Sprintf(
+			"{\"level\": \"%q\", \"module\": \"metalog\", \"message\": \"failed to convert log message to JSON\"}\n",
+			LevelFormatter(levelERROR),
+		)
 	}
 
 	return string(jsonLog) + "\n"
